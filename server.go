9215c64e455af7812d03f640287bc9be23e82c43
Kohei SUGI <koheisg@hey.com>
2024-09-26 20:28:33 +0900

    Add /users/:id endpoint

--- server.go
+++ server.go
@@ -12,4 +12,12 @@ func main() {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
+	e.GET("/users/:id", getUser)
+}
+
+// e.GET("/users/:id", getUser)
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
 }
